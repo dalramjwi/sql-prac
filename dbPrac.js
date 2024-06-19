61dae242c5e7c3851d8ce3da8b5ee6d0669ff43e
dalramjwi <dalramjwi@gmail.com>
2024-06-19 23:52:57 +0900

    create start, question to run and prepare

--- dbPrac.js
+++ dbPrac.js
@@ -2,4 +2,8 @@ const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("./database/prac.db");
 const mainTable = () => {
   // const dbCheck = db.all
+  const createDB = () => {
+    //같이 할 때는 run, 다시 할 때는 prepare 무슨 의미인지 조사해보자
+    db.run("CREATE TABLE ");
+  };
 };
