de8815ae57542772667b2f00e57cb9e33e1aa53c
dalramjwi <dalramjwi@gmail.com>
2024-06-19 14:46:48 +0900

    update success

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const db = require("sqlite3").verbose();
 const sqlite3 = new db.Database("./database/prac.db");
 // sqlite3.run("CREATE TABLE zootopia(id interger, name text not null)");
-const insert = sqlite3.prepare("INSERT INTO zootopia (id, name) VALUES (?,?)");
-insert.run("1", "김보미");
-insert.finalize();
+// const insert = sqlite3.prepare("INSERT INTO zootopia (id, name) VALUES (?,?)");
+// insert.run("1", "김보미");
+// insert.finalize();
 sqlite3.all("SELECT * FROM zootopia", (err, rows) => {
   if (err) {
     console.log(err);
@@ -12,3 +12,6 @@ sqlite3.all("SELECT * FROM zootopia", (err, rows) => {
     console.log(`${rows.id} : ${rows.name}`);
   });
 });
+const update = sqlite3.prepare("UPDATE zootopia SET name =? WHERE id =?");
+update.run("김달", "1");
+update.finalize();
