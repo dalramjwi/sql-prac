13ac84c96ec20c8a474f744b82276efcf42c4a03
dalramjwi <dalramjwi@gmail.com>
2024-06-19 10:08:53 +0900

    app.js make and past file delete

--- server.js
+++
@@ -1,6 +0,0 @@
-const sqlite3 = require("sqlite3").verbose();
-let db = new sqlite3.Database("./database/index.db");
-db.run(
-  "CREATE TABLE student(id integer primary key, name text not null, email text unique)"
-);
-db.close();
