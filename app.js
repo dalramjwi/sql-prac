e0163fe16cd2ad7ff1294cd6b7cb1917d74c6e9f
dalramjwi <dalramjwi@gmail.com>
2024-06-19 14:34:37 +0900

    db insert and read

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const db = require("sqlite3").verbose();
 const sqlite3 = new db.Database("./database/prac.db");
-sqlite3.run("CREATE TABLE zootopia(id interger, name text not null)");
+// sqlite3.run("CREATE TABLE zootopia(id interger, name text not null)");
+const insert = sqlite3.prepare("INSERT INTO zootopia (id, name) VALUES (?,?)");
+insert.run("1", "김보미");
+insert.finalize();
+db.all("SELECT * FROM zootopia", (err, rows) => {
+  if (err) {
+    console.log(err);
+  }
+  rows.forEach((rows) => {
+    console.log(`${rows.id} : ${rows.name}`);
+  });
+});
